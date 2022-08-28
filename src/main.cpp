bfa72d343caf0395a4c20670c78a95b2e97b0404
Ajay K <->
2022-08-29 05:18:17 +0530

    Added option to print usage message.

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,25 @@
 #include "game.h"
 #include "hilo_config.h"
 #include<iostream>
+#include<string>
+#include <iomanip>
 
-int main()
+static void print_info()
 {
     std::cout<<"Hilo v"<<Hilo_VERSION_MAJOR<<'.'
-    <<Hilo_VERSION_MINOR<<'.'<<Hilo_VERSION_PATCH<<'\n';
-    run_game();
+             <<Hilo_VERSION_MINOR<<'.'<<Hilo_VERSION_PATCH<<'\n';
+    std::cout<<"Usage: hilo\n"
+    <<std::setw(16)<<"hilo run\n";
+}
+int main(int argc,const char** argv)
+{
+    if(argc==1 || argv[1]==std::string{"run"})
+    {
+        run_game();
+    }
+    else
+    {
+        print_info();
+    }
     return 0;
 }
