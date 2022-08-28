c3c642083642555a6127b0d8ac485a9357e42885
Ajay K <->
2022-08-28 15:55:22 +0530

    Added project config file to access project version from source code.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 
 #include "game.h"
+#include "config/hilo_config.h"
+#include<iostream>
 
 int main()
 {
+    std::cout<<"Hilo v"<<Hilo_VERSION_MAJOR<<'.'
+    <<Hilo_VERSION_MINOR<<'.'<<Hilo_VERSION_PATCH<<'\n';
     run_game();
     return 0;
 }
